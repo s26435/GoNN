79d6544921a09b07668190640c5a6c17f1a65303
s26435 <s26435@pjwstk.edu.pl>
2024-06-21 12:45:39 +0200

    feat: neural network

--- main.go
+++ main.go
@@ -5,49 +5,100 @@ import (
 	"encoding/csv"
 	"fmt"
 	"log"
+	"math"
+	"math/rand"
 	"os"
 	"strconv"
 )
 
-type Data struct {
-	patientID          uint16
-	age                uint8
-	gender             bool
-	ethnicity          uint8
-	education          uint8
-	BMI                float64
-	smoking            bool
-	alcoholConsumption float64
-	physicalActivity   float64
-	dietQuality        float64
-	sleepQuality       float64
-	familyAlzheimers   bool
-	cardioDisease      bool
-	diabetes           bool
-	depression         bool
-	headInjury         bool
-	hypertension       bool
-	systolicBP         uint
-	diastolicBP        uint
-	cholesterolTotal   float64
-	cholesterolLDL     float64
-	cholesterolHDL     float64
-	triglycerides      float64
-	MMSE               float64
-	funcAssessment     float64
-	memoryComplaints   bool
-	behavioralProb     bool
-	ADL                float64
-	confusion          bool
-	disorientation     bool
-	personalityChanges bool
-	difficultyTask     bool
-	forgetfulness      bool
-	diagnosis          bool
-}
+type (
+	Data struct {
+		patientID uint16
+		//demographic details
+		age       uint8 // 60 - 90 years
+		gender    bool  // tru = female, false = male
+		ethnicity uint8 // 0: caucasian, 1:afroamerican, 2:asian, 3: other
+		education uint8 // 0:none, 1:high school, 2: belchelor's 3: higher
+		// lifestyle factors
+		BMI                float64 // 15 -40
+		smoking            bool
+		alcoholConsumption float64 // weekly alcohol consumption
+		physicalActivity   float64 // weekly physical activity 0-10
+		dietQuality        float64 // 0 - 10	(grade)
+		sleepQuality       float64 // 4 - 10
+		//medical history
+		familyAlzheimers bool // alzhaimers in family history
+		cardioDisease    bool
+		diabetes         bool
+		depression       bool
+		headInjury       bool
+		hypertension     bool
+		//clinical measurements
+		systolicBP       uint
+		diastolicBP      uint
+		cholesterolTotal float64
+		cholesterolLDL   float64
+		cholesterolHDL   float64
+		triglycerides    float64
+		//cognitive and functional assessments
+		MMSE             float64 //mini-mental state examination score 0 - 30
+		funcAssessment   float64 // 0-10
+		memoryComplaints bool
+		behavioralProb   bool
+		ADL              float64 //activities of daily living score 0 - 10
+		//symptoms
+		confusion          bool
+		disorientation     bool
+		personalityChanges bool
+		difficultyTask     bool
+		forgetfulness      bool
+		//DIAGNOSIS
+		diagnosis bool
+	}
+)
 
 type DataFrame struct {
-	frame []Data
+	patientID []uint16
+	//demographic details
+	age       []uint8 // 60 - 90 years
+	gender    []bool  // tru = female, false = male
+	ethnicity []uint8 // 0: caucasian, 1:afroamerican, 2:asian, 3: other
+	education []uint8 // 0:none, 1:high school, 2: belchelor's 3: higher
+	// lifestyle factors
+	BMI                []float64 // 15 -40
+	smoking            []bool
+	alcoholConsumption []float64 // weekly alcohol consumption
+	physicalActivity   []float64 // weekly physical activity 0-10
+	dietQuality        []float64 // 0 - 10	(grade)
+	sleepQuality       []float64 // 4 - 10
+	//medical history
+	familyAlzheimers []bool // alzhaimers in family history
+	cardioDisease    []bool
+	diabetes         []bool
+	depression       []bool
+	headInjury       []bool
+	hypertension     []bool
+	//clinical measurements
+	systolicBP       []uint
+	diastolicBP      []uint
+	cholesterolTotal []float64
+	cholesterolLDL   []float64
+	cholesterolHDL   []float64
+	triglycerides    []float64
+	//cognitive and functional assessments
+	MMSE             []float64 //mini-mental state examination score 0 - 30
+	funcAssessment   []float64 // 0-10
+	memoryComplaints []bool
+	behavioralProb   []bool
+	ADL              []float64 //activities of daily living score 0 - 10
+	//symptoms
+	confusion          []bool
+	disorientation     []bool
+	personalityChanges []bool
+	difficultyTask     []bool
+	forgetfulness      []bool
+	//DIAGNOSIS
+	diagnosis []bool
 }
 
 func (data Data) Print() {
@@ -97,15 +148,11 @@ func (df *DataFrame) loadData(path string) {
 			log.Fatal(err)
 		}
 	}()
-
 	reader := csv.NewReader(bufio.NewReader(file))
-
-	// Skip the header row
 	_, err = reader.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for {
 		record, err := reader.Read()
 		if err != nil {
@@ -116,44 +163,40 @@ func (df *DataFrame) loadData(path string) {
 			log.Fatalf("Expected 35 fields, but got %d", len(record))
 		}
 
-		data := Data{
-			patientID:          parseUint16(record[0]),
-			age:                parseUint8(record[1]),
-			gender:             parseBool(record[2]),
-			ethnicity:          parseUint8(record[3]),
-			education:          parseUint8(record[4]),
-			BMI:                parseFloat64(record[5]),
-			smoking:            parseBool(record[6]),
-			alcoholConsumption: parseFloat64(record[7]),
-			physicalActivity:   parseFloat64(record[8]),
-			dietQuality:        parseFloat64(record[9]),
-			sleepQuality:       parseFloat64(record[10]),
-			familyAlzheimers:   parseBool(record[11]),
-			cardioDisease:      parseBool(record[12]),
-			diabetes:           parseBool(record[13]),
-			depression:         parseBool(record[14]),
-			headInjury:         parseBool(record[15]),
-			hypertension:       parseBool(record[16]),
-			systolicBP:         parseUint(record[17]),
-			diastolicBP:        parseUint(record[18]),
-			cholesterolTotal:   parseFloat64(record[19]),
-			cholesterolLDL:     parseFloat64(record[20]),
-			cholesterolHDL:     parseFloat64(record[21]),
-			triglycerides:      parseFloat64(record[22]),
-			MMSE:               parseFloat64(record[23]),
-			funcAssessment:     parseFloat64(record[24]),
-			memoryComplaints:   parseBool(record[25]),
-			behavioralProb:     parseBool(record[26]),
-			ADL:                parseFloat64(record[27]),
-			confusion:          parseBool(record[28]),
-			disorientation:     parseBool(record[29]),
-			personalityChanges: parseBool(record[30]),
-			difficultyTask:     parseBool(record[31]),
-			forgetfulness:      parseBool(record[32]),
-			diagnosis:          parseBool(record[33]),
-		}
-
-		df.frame = append(df.frame, data)
+		df.patientID = append(df.patientID, parseUint16(record[0]))
+		df.age = append(df.age, parseUint8(record[1]))
+		df.gender = append(df.gender, parseBool(record[2]))
+		df.ethnicity = append(df.ethnicity, parseUint8(record[3]))
+		df.education = append(df.education, parseUint8(record[4]))
+		df.BMI = append(df.BMI, parseFloat64(record[5]))
+		df.smoking = append(df.smoking, parseBool(record[6]))
+		df.alcoholConsumption = append(df.alcoholConsumption, parseFloat64(record[7]))
+		df.physicalActivity = append(df.physicalActivity, parseFloat64(record[8]))
+		df.dietQuality = append(df.dietQuality, parseFloat64(record[9]))
+		df.sleepQuality = append(df.sleepQuality, parseFloat64(record[10]))
+		df.familyAlzheimers = append(df.familyAlzheimers, parseBool(record[11]))
+		df.cardioDisease = append(df.cardioDisease, parseBool(record[12]))
+		df.diabetes = append(df.diabetes, parseBool(record[13]))
+		df.depression = append(df.depression, parseBool(record[14]))
+		df.headInjury = append(df.headInjury, parseBool(record[15]))
+		df.hypertension = append(df.hypertension, parseBool(record[16]))
+		df.systolicBP = append(df.systolicBP, parseUint(record[17]))
+		df.diastolicBP = append(df.diastolicBP, parseUint(record[18]))
+		df.cholesterolTotal = append(df.cholesterolTotal, parseFloat64(record[19]))
+		df.cholesterolLDL = append(df.cholesterolLDL, parseFloat64(record[20]))
+		df.cholesterolHDL = append(df.cholesterolHDL, parseFloat64(record[21]))
+		df.triglycerides = append(df.triglycerides, parseFloat64(record[22]))
+		df.MMSE = append(df.MMSE, parseFloat64(record[23]))
+		df.funcAssessment = append(df.funcAssessment, parseFloat64(record[24]))
+		df.memoryComplaints = append(df.memoryComplaints, parseBool(record[25]))
+		df.behavioralProb = append(df.behavioralProb, parseBool(record[26]))
+		df.ADL = append(df.ADL, parseFloat64(record[27]))
+		df.confusion = append(df.confusion, parseBool(record[28]))
+		df.disorientation = append(df.disorientation, parseBool(record[29]))
+		df.personalityChanges = append(df.personalityChanges, parseBool(record[30]))
+		df.difficultyTask = append(df.difficultyTask, parseBool(record[31]))
+		df.forgetfulness = append(df.forgetfulness, parseBool(record[32]))
+		df.diagnosis = append(df.diagnosis, parseBool(record[33]))
 	}
 }
 
@@ -197,15 +240,178 @@ func parseBool(s string) bool {
 	return v
 }
 
-func (df DataFrame) Print() {
-	for _, data := range df.frame {
-		data.Print()
-		fmt.Println("-------------")
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+func sigmoidDerivative(x float64) float64 {
+	return x * (1.0 - x)
+}
+
+type NeuralNetwork struct {
+	inputSize  int
+	hiddenSize int
+	outputSize int
+
+	weightsInputHidden  [][]float64
+	weightsHiddenOutput []float64
+	hiddenLayerInput    []float64
+	hiddenLayerOutput   []float64
+
+	outputLayerInput float64
+	output           float64
+}
+
+func newNeuralNetwork(inputSize, hiddenSize, outputSize int) NeuralNetwork {
+	tempWeightsInputHidden := make([][]float64, inputSize)
+	for i := range tempWeightsInputHidden {
+		tempWeightsInputHidden[i] = make([]float64, hiddenSize)
+	}
+	tempWeightsHiddenOutput := make([]float64, hiddenSize)
+
+	for i := range tempWeightsInputHidden {
+		for j := range tempWeightsInputHidden[i] {
+			tempWeightsInputHidden[i][j] = rand.Float64()
+		}
+	}
+
+	for i := range tempWeightsHiddenOutput {
+		tempWeightsHiddenOutput[i] = rand.Float64()
+	}
+
+	return NeuralNetwork{hiddenSize: hiddenSize,
+		inputSize:           inputSize,
+		outputSize:          outputSize,
+		weightsInputHidden:  tempWeightsInputHidden,
+		weightsHiddenOutput: tempWeightsHiddenOutput,
+		outputLayerInput:    0,
+		output:              0,
 	}
 }
 
+func (nn *NeuralNetwork) forward(input []float64) float64 {
+	nn.hiddenLayerInput = make([]float64, nn.hiddenSize)
+	nn.hiddenLayerOutput = make([]float64, nn.hiddenSize)
+	for i := 0; i < nn.hiddenSize; i++ {
+		nn.hiddenLayerInput[i] = 0.0
+		for j := 0; j < nn.inputSize; j++ {
+			nn.hiddenLayerInput[i] += input[j] * nn.weightsInputHidden[j][i]
+		}
+		nn.hiddenLayerOutput[i] = sigmoid(nn.hiddenLayerInput[i])
+	}
+	nn.outputLayerInput = 0.0
+	for i := 0; i < nn.hiddenSize; i++ {
+		nn.outputLayerInput += nn.hiddenLayerOutput[i] * nn.weightsHiddenOutput[i]
+	}
+
+	nn.output = sigmoid(nn.outputLayerInput)
+
+	return nn.output
+}
+
+func (nn NeuralNetwork) backward(input []float64, target, learningRate float64) {
+	var outputError float64 = target - nn.output
+	var outputDelta float64 = outputError * sigmoidDerivative(nn.output)
+
+	hiddenError := make([]float64, nn.hiddenSize)
+	hiddenDelta := make([]float64, nn.hiddenSize)
+
+	for i := 0; i < nn.hiddenSize; i++ {
+		hiddenError[i] = outputDelta * nn.weightsHiddenOutput[i]
+		hiddenDelta[i] = hiddenError[i] * sigmoidDerivative(nn.hiddenLayerOutput[i])
+	}
+	for i := 0; i < nn.hiddenSize; i++ {
+		nn.weightsHiddenOutput[i] += nn.hiddenLayerOutput[i] * outputDelta * learningRate
+	}
+
+	for i := 0; i < nn.hiddenSize; i++ {
+		for j := 0; j < nn.hiddenSize; j++ {
+			nn.weightsInputHidden[i][j] += input[i] * hiddenDelta[i] * learningRate
+		}
+	}
+}
+
+func boolToFloat(x bool) float64 {
+	if x {
+		return 1.0
+	} else {
+		return 0.0
+	}
+}
+
+func (nn NeuralNetwork) loss(target float64) float64 {
+	loss := math.Pow(target-nn.output, 2)
+	return loss
+}
+
+func averageLoss(lossTable []float64) float64 {
+	var result float64
+	for i := range lossTable {
+		result += lossTable[i]
+	}
+	result = result / float64(len(lossTable))
+	var varx float64
+	for i := range lossTable {
+		varx = math.Pow(lossTable[i]-result, 2)
+	}
+	return math.Sqrt(varx / float64(len(lossTable)))
+}
+
 func main() {
-	dataFrame := DataFrame{}
-	dataFrame.loadData("alzheimers_disease_data.csv")
-	dataFrame.Print()
+	inputSize := 31
+	hiddenSize := 10
+	outputSize := 1
+	learningRate := 0.1
+	epochs := 10
+	nn := newNeuralNetwork(inputSize, hiddenSize, outputSize)
+
+	var data DataFrame
+	data.loadData("alzheimers_disease_data.csv")
+	for j := 0; j < epochs; j++ {
+		var lossTable []float64
+		for i := range data.patientID {
+			var d []float64
+			d = append(d, float64(data.age[i]))
+			d = append(d, boolToFloat(data.gender[i]))
+			d = append(d, float64(data.ethnicity[i]))
+			d = append(d, float64(data.education[i]))
+			d = append(d, data.BMI[i])
+			d = append(d, boolToFloat(data.smoking[i]))
+			d = append(d, data.alcoholConsumption[i])
+			d = append(d, data.physicalActivity[i])
+			d = append(d, data.dietQuality[i])
+			d = append(d, data.sleepQuality[i])
+			d = append(d, boolToFloat(data.familyAlzheimers[i]))
+			d = append(d, boolToFloat(data.cardioDisease[i]))
+			d = append(d, boolToFloat(data.diabetes[i]))
+			d = append(d, boolToFloat(data.headInjury[i]))
+			d = append(d, boolToFloat(data.hypertension[i]))
+			d = append(d, float64(data.systolicBP[i]))
+			d = append(d, float64(data.diastolicBP[i]))
+			d = append(d, data.cholesterolTotal[i])
+			d = append(d, data.cholesterolLDL[i])
+			d = append(d, data.cholesterolHDL[i])
+			d = append(d, data.triglycerides[i])
+			d = append(d, data.MMSE[i])
+			d = append(d, data.funcAssessment[i])
+			d = append(d, boolToFloat(data.memoryComplaints[i]))
+			d = append(d, boolToFloat(data.behavioralProb[i]))
+			d = append(d, data.ADL[i])
+			d = append(d, boolToFloat(data.confusion[i]))
+			d = append(d, boolToFloat(data.disorientation[i]))
+			d = append(d, boolToFloat(data.personalityChanges[i]))
+			d = append(d, boolToFloat(data.difficultyTask[i]))
+			d = append(d, boolToFloat(data.forgetfulness[i]))
+			nn.forward(d)
+			nn.backward(d, boolToFloat(data.diagnosis[i]), learningRate)
+			target := boolToFloat(data.diagnosis[i])
+			loss := nn.loss(target)
+			//fmt.Println(loss)
+			nn.backward(d, target, learningRate)
+			lossTable = append(lossTable, loss)
+		}
+		if j%1 == 0 {
+			fmt.Printf("Epoch: %d Loss: %f \n", j, averageLoss(lossTable))
+		}
+	}
 }
